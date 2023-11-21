0c99abe047a81c178f432075c4ab14385b0fb0c5
Mehmet <mehmetsonmez216@gmail.com>
2023-11-22 01:25:07 +0300

    Bed, Lamp, LampType created

---
+++ src/com/workintech/rooms/Lamp.java
@@ -0,0 +1,35 @@
+package com.workintech.rooms;
+
+import com.workintech.enums.LampType;
+
+public class Lamp {
+    private LampType style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(LampType style,
+                boolean battery,
+                int globRating){
+        this.style=style;
+        this.battery=battery;
+        this.globRating=globRating;
+    }
+
+    public LampType getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+
+    public void turnOn(){
+        System.out.println("Lamp is being turned on");
+    }
+
+
+}
